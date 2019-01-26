0a7f68f9b33cc47478e50162d09a22c8d1a16f6a
Carlos Vences <cvences@c.teamexos.com>
2019-01-26 10:13:43 -0600

    Agregamos docstrings renombramos lista de prueba utilizamos list comprehension agregamos try/except

--- heapSort.py
+++ heapSort.py
@@ -1,15 +1,30 @@
-a = [125, 84, 52, 13, 29, 55, 54, 56, ]
+"""
+Ordenamiento HeapSort
+
+"""
+
+
+lista_prueba = [125, 84, 52, 13, 29, 55, 54, 56, ]
 
 
 def swap(a, i, j):
+    """
+    intercambia valores del arreglo a, en las posiciones i y j
+
+    """
     a[int(i)], a[int(j)] = a[int(j)], a[int(i)]
 
 
 def is_heap(a):
+    """
+    checa si los dos elementos del arreglo son un heap
+
+    """
     n = 0
     m = 0
+    # ciclo infinito, se rompe cuando alguna de las dos condiciones se cumple (líneas 29 y 31)
     while True:
-        for i in [0, 1, ]:
+        for i in range(1):
             m += 1
             if m >= len(a):
                 return True
@@ -18,14 +33,20 @@ def is_heap(a):
         n += 1
 
 
-def sift_down(a, n, max):
+def shift_down(a, n, maximo):
     while True:
         biggest = n
         c1 = 2 * n + 1
         c2 = c1 + 1
-        for c in [c1, c2]:
-            if c < max and a[int(c)] > a[int(biggest)]:
-                biggest = c
+        # biggest = [c for c in [c1, c2, ] if c < maximo and a[int(c)] > a[int(biggest)]]
+        # list comprehension
+        posicion_del_mayor = [c for c in [c1, c2, ] if c < maximo and a[int(c)] > a[int(biggest)]]
+        try:
+            biggest = posicion_del_mayor.pop()
+        except IndexError:
+            # posicion_del_mayor = []
+            pass
+
         if biggest == n:
             return
         swap(a, n, biggest)
@@ -36,7 +57,7 @@ def heapify(a):
     i = len(a) / 2 - 1
     max = len(a)
     while i >= 0:
-        sift_down(a, i, max)
+        shift_down(a, i, max)
         i -= 1
 
 
@@ -45,9 +66,9 @@ def heapsort(a):
     j = len(a) - 1
     while j > 0:
         swap(a, 0, j)
-        sift_down(a, 0, j)
+        shift_down(a, 0, j)
         j -= 1
 
 
-heapsort(a)
-print(a)
+heapsort(lista_prueba)
+print(lista_prueba)
