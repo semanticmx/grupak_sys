b2f6c3ed202bbd5e98884231a94cbf2a53d34d4a
Carlos Vences <cvences@c.teamexos.com>
2019-02-02 10:07:22 -0600

    Agregando insercion de https://github.com/usky12/grupakpython

---
+++ insercion.py
@@ -0,0 +1,22 @@
+lista_prueba = [6, 5, 3, 1, 8, 7, 2, 4, ]
+
+
+def ordenamiento_insercion(lista_n):
+    """
+    implementación del ordenamiento de inserción
+
+    """
+    for i in range(len(lista_n)):
+        minimo = i
+
+        for j in range(i, len(lista_n)):
+            if lista_n[j]<lista_n[minimo]:
+                minimo = j
+        if minimo != i:
+            lista_n[i], lista_n[minimo] = lista_n[minimo], lista_n[i]
+
+    return lista_n
+
+
+print(lista_prueba)
+print(ordenamiento_insercion(lista_prueba))
