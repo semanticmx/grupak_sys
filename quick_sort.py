02a1174e8a027f5ba4983796cc599eb7f247f8d1
Carlos Vences <cvences@c.teamexos.com>
2019-02-02 09:51:07 -0600

    Agregar docstring

--- quick_sort.py
+++ quick_sort.py
@@ -6,10 +6,14 @@ mi_lista = [n for n in range(max_items)]
 shuffle(mi_lista)
 
 
-def sort(lista):
-    izquierda = []
-    centro = []
-    derecha = []
+def quick_sort(lista):
+    """
+    implementación del método de ordenamiento quick sort.
+    compara 3 listas y las concatena, de manera recursiva.
+
+    """
+    izquierda, centro, derecha = [], [], []
+
     if len(lista) > 1:
         pivote = lista[0]
         for i in lista:
@@ -19,13 +23,13 @@ def sort(lista):
                 centro.append(i)
             elif i > pivote:
                 derecha.append(i)
-        return sort(izquierda)+centro+sort(derecha)
+        return quick_sort(izquierda) + centro + quick_sort(derecha)
     else:
         return lista
 
 
 start_time = time()
-lista_ordenada = sort(mi_lista)
+lista_ordenada = quick_sort(mi_lista)
 stop_time = time()
 
 print(f'Lista: {lista_ordenada}, ordenada en {stop_time - start_time}')
